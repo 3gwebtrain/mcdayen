22206730048be5e2149f77ce75b587910765eceb
3gwebtrain <3gwebtrain@gmail.com>
2023-01-08 14:26:19 +0530

    footer added

--- libs/components/src/lib/footer/footer.tsx
+++ libs/components/src/lib/footer/footer.tsx
@@ -7,6 +7,7 @@ export function Footer(props: FooterProps) {
     return (
         <div className={styles['container']}>
             <h1>Welcome to Footer!</h1>
+            <div className="bg-stone-800 p-2 font-mono">Hello from footer!</div>
         </div>
     );
 }
