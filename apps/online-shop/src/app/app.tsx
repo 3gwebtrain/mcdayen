88d33f7b7ce1c6ecc2bebcbd3f958ee60562f134
3gwebtrain <3gwebtrain@gmail.com>
2023-01-08 13:21:47 +0530

    header and footer created under component library

--- apps/online-shop/src/app/app.tsx
+++ apps/online-shop/src/app/app.tsx
@@ -1,12 +1,16 @@
+import { Footer, Header } from '@mcdayen/components';
+
 export function App() {
     return (
         <div>
             empty
+            <Header />
             <picture>
                 <source media="(max-width:768px)" srcSet="/assets/mobile/fashion_front.jpeg"></source>
                 <source media="(min-width:769px)" srcSet="/assets/desktop/fashion_front.jpeg"></source>
                 <img alt="women" src="/assets/desktop/fashion_front.jpeg" />
             </picture>
+            <Footer />
         </div>
     );
 }
