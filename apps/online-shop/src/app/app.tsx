32de71bc589bfbc2af350cfbd25ff6729665b518
3gwebtrain <3gwebtrain@gmail.com>
2023-01-08 19:23:24 +0530

    basic layout added

--- apps/online-shop/src/app/app.tsx
+++ apps/online-shop/src/app/app.tsx
@@ -7,13 +7,13 @@ const nameData: NameProps = {
 
 export function App() {
     return (
-        <div>
-            <div className="bg-indigo-500 p-2 font-mono">Hello! {nameData.name}</div>
+        <div className="flex flex-col border-solid border-2 border-green-200 min-h-screen flex-wrap">
             <Header />
+            <div className="bg-indigo-500 p-2 text-xs">Hello! {nameData.name}</div>
             <picture>
-                <source media="(max-width:768px)" srcSet="/assets/mobile/fashion_front.jpeg"></source>
-                <source media="(min-width:769px)" srcSet="/assets/desktop/fashion_front.jpeg"></source>
-                <img alt="women" src="/assets/desktop/fashion_front.jpeg" />
+                <source media="(max-width:768px)" srcSet="/assets/mobile/model_front.jpeg"></source>
+                <source media="(min-width:769px)" srcSet="/assets/desktop/model_front.jpeg"></source>
+                <img alt="women" src="/assets/desktop/model_front.jpeg" />
             </picture>
             <Footer />
         </div>
