4f246889845f314483e90030e618e5b05a71673e
3gwebtrain <3gwebtrain@gmail.com>
2023-01-12 20:34:50 +0530

    key addeed with size

--- libs/micro-components/src/lib/size-tag/size-tag.tsx
+++ libs/micro-components/src/lib/size-tag/size-tag.tsx
@@ -3,7 +3,7 @@ import { sizeProps } from "@mcdayen/prop-types";
 export function SizeTag({value}:{value:sizeProps[]}) {
     return (
         <div>
-            {value.map((v) => <span>{v.size}</span>)}
+            {value.map((v) => <span key={v.id}>{v.size}</span>)}
         </div>
     );
 }
