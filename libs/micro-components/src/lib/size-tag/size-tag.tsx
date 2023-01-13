9a94dbf49accfa5215ae8bba97ae851f7d6189e1
3gwebtrain <3gwebtrain@gmail.com>
2023-01-13 10:55:49 +0530

    key added

--- libs/micro-components/src/lib/size-tag/size-tag.tsx
+++ libs/micro-components/src/lib/size-tag/size-tag.tsx
@@ -7,7 +7,7 @@ export function SizeTag({ value }: { value: sizeProps[] }) {
     }
     return (
         <div className="flex gap-2 flex-wrap flex-justify mb-6">
-            {value.map((v) => <span className="w-1/5">
+            {value.map((v) => <span className="w-1/5" key={v.id}>
                 <button
                     onClick={() => setSize(v)}
                     className="rounded-full border-2 border-gray-600/[.55]  text-xs px-4 py-1 hover:bg-black hover:text-white" key={v.id}>{v.size}</button>
